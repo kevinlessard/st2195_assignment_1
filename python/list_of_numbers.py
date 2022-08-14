7800ffb9aa0b8234438ed412fd2574f476d78e42
kevinlessard <lessardkj@gmail.com>
2022-08-14 13:51:59 -0600

    adding python folder and script

---
+++ python/list_of_numbers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 14 13:44:29 2022
+
+@author: kevinlessard
+"""
+
+import numpy as np
+
+list1 = [1,2,3,4,5,6,7,8,9,10]
+
+vector1 = np.array(list1)
+print(vector1)
